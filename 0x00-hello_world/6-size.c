67bec0f0307ad4d1e32d53a9cec8e408b8f52914
jihuder <julioarenas85@gmail.com>
2020-09-26 12:27:12 -0500

    added 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+float main(void)
+{
+char a = 'z';
+int b = 2147483647;
+shot c = 32767;
+float d = 3.4E38;
+double e = 1.7E308;
+printf("Valor de a=%d bytes\nValor de b=%d bytes\nValor de c=%d bytes\nValor de d=%d bytes\nValor de e=%d", sizeof(a), sizeof(b), sizeof(c), sizeof(d), sizeof(e));
+return (0);
+}
