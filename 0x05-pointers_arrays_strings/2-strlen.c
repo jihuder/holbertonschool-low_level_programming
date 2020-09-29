c873bb519eac27bdf1760a6ca27328c260ed9838
jihuder <julioarenas85@gmail.com>
2020-09-29 07:24:13 -0500

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen - return string length
+ * @s: varible string
+ * Return: i
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
+}
