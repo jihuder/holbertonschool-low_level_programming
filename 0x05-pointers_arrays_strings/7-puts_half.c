582510a1f0f8ceb114cf03cfd44c2364e9a506f8
jihuder <julioarenas85@gmail.com>
2020-10-01 22:56:37 -0500

    added 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,14 @@
 void puts_half(char *str)
 {
 int i = 0;
-int div ;
-for (i = 0; str[i] != '\0';i++)
+int div;
+for (i = 0; str[i] != '\0';  i++)
 {
 i++;
 div = i / 2;
 div = i - div;
 }
-while(str[div] != '\0')
+while (str[div] != '\0')
 {
 div++;
 _putchar(str[div]);
