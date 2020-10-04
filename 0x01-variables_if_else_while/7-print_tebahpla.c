05582d475ff5c9b38af27c67cd3ec5b12b174360
jihuder <julioarenas85@gmail.com>
2020-10-03 23:50:19 -0500

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This program prints the alphabet in reverse.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
