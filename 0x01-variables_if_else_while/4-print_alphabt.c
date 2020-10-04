bcfc9372265a3b8d3ba5ab3de47f1113cb9cf43c
jihuder <julioarenas85@gmail.com>
2020-10-03 23:07:45 -0500

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+/**
+ * main - no eq
+ *
+ *Return: álways
+ */
+int main(void)
+{
+  char al;
+
+  for (al = 'a'; al <= 'z'; al++)
+    {
+      if (!(al == 'e' || al == 'q'))
+	{
+	  putchar(al);
+	}
+    }
+  putchar('\n');
+  return (0);
+}
