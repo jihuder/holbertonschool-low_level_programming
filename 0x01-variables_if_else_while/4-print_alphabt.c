df6079d30512b966c8e409dfe29fd112c631d3ff
jihuder <julioarenas85@gmail.com>
2020-10-03 23:11:17 -0500

    added 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-  char al;
-
-  for (al = 'a'; al <= 'z'; al++)
-    {
-      if (!(al == 'e' || al == 'q'))
-	{
-	  putchar(al);
-	}
-    }
-  putchar('\n');
-  return (0);
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+if (!(al == 'e' || al == 'q'))
+{
+putchar(al);
+}
+}
+putchar('\n');
+return (0);
 }
