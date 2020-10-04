a38352e5bd87845adfaa1de7ab3935905e1f22fb
jihuder <julioarenas85@gmail.com>
2020-10-03 22:59:42 -0500

    added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Alphabet printing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letra = 97;
+while (letra < 123)
+{
+putchar(letra);
+letra++;
+}
+letra = 65;
+while (letra < 91)
+{
+putchar(letra);
+letra++;
+}
+putchar('\n');
+return (0);
+}
