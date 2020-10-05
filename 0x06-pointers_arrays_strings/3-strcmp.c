3becc9dff8ee622b69ac2f1b8c5beff364c9c183
jihuder <julioarenas85@gmail.com>
2020-10-04 23:09:36 -0500

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcmp - check the code for Holberton School students.
+ * @s1: this is the first accountant.
+ * @s2: this is the second accountant.
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int ax;
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+if (s1[i] == s2[i])
+{
+ax = 0;
+}
+else
+{
+ax = s1[i] - s2[i];
+break;
+}
+}
+return (ax);
+}
