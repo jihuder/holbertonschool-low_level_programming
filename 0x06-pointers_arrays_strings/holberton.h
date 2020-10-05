fe636d7776ecc2dee5671c78e857ce781a4ba97a
jihuder <julioarenas85@gmail.com>
2020-10-04 22:56:51 -0500

    update holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,8 +1,8 @@
-#ifndef HEADER_FILE
+ #ifndef HEADER_FILE
 #define HEADER_FILE
-
 int _putchar(char c);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char*dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
