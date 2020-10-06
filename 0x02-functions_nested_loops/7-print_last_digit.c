d645d86b2c0a1d4e6104d660a602d7fc830dd671
jihuder <julioarenas85@gmail.com>
2020-10-06 14:29:43 -0500

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_last_digit - unction that computes the absolute value of an integer.
+ *@n: this variable
+ *Return: the content saved in variable n
+ */
+int print_last_digit(int n)
+{
+int a;
+a = n % 10;
+if (a < 0)
+{
+_putchar('0' + (-a));
+return ((-a));
+}
+_putchar('0' + a);
+return (a);
+}
