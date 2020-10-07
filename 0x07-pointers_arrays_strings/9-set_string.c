8d2bcd6eeb4aee892c5ff6385d798c1a162bd2c9
jihuder <julioarenas85@gmail.com>
2020-10-07 11:24:31 -0500

    added 9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - main function.
+ * @s: pointer to pointer.
+ * @to: pointer to pointer.
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
