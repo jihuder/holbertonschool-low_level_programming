5f9968228e4884577a0fce932e84f24817a443b2
jihuder <julioarenas85@gmail.com>
2020-10-07 11:06:17 -0500

    added 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *_strstr - main funtion.
+ * @haystack: memory area
+ * @needle: another memory area.
+ * Return: always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char *c, *a;
+while (*haystack != '\0')
+{
+c = haystack;
+a = needle;
+while (*a == *haystack && *a != '\0'
+&& *haystack != '\0')
+{
+haystack++;
+a++;
+}
+if (*a == '\0')
+return (c);
+haystack = c + 1;
+}
+return (NULL);
+}
