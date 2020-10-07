61648b0444a31bc2b91af47088fd9b43e0fdef34
jihuder <julioarenas85@gmail.com>
2020-10-07 13:24:55 -0500

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints the string "holberton" from a character array
+ * Return: 0 on success
+ */
+void print_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
