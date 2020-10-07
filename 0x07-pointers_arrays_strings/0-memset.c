6340d2ce9928ffe389ba9d9cc00c8710f596ba74
jihuder <julioarenas85@gmail.com>
2020-10-07 01:51:15 -0500

    added 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *_memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: the size of the memory to print
+ * Return: s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int count;
+for (count = 0; count < n; count++)
+{
+s[count] = b;
+}
+return (s);
+}
