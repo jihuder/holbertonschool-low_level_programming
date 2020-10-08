05edee9bf72702607388080c7cde75f6750147cb
jihuder <julioarenas85@gmail.com>
2020-10-07 23:13:48 -0500

    added 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - function
+ * @x: return address
+ * @y: return address
+ * Return: s.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
