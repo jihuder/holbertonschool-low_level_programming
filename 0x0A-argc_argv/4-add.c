1847c9c54fce7b17fbaa9e0907d407aaf197384e
jihuder <julioarenas85@gmail.com>
2020-10-19 10:31:49 -0500

    added 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: Argument vector contains the arguments passed to the program
+ * Return: Always 0 (Success) and exit
+ */
+int main(int argc, char *argv[])
+{
+int c, j, r = 0;
+
+if (argc > 0)
+{
+for (c = 1; c < argc; c++)
+{
+for (j = 0; argv[c][j] != '\0'; j++)
+{
+if (!(isdigit(argv[c][j])))
+{
+printf("Error\n");
+return (1);
+}
+}
+r += atoi(argv[c]);
+}
+printf("%d\n", r);
+}
+else
+printf("0\n");
+return (0);
+}
