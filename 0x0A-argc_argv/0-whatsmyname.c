ef9e0eb50ddfef4e616d22a85cc3ea0fa6391008
jihuder <julioarenas85@gmail.com>
2020-10-19 04:18:13 -0500

    added 0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/ **
+ * main - function
+ * @argc: matrix of pointers
+ * @argv: saving the number of arguments
+ * Return: 0
+ */
+
+int  main(int argc __attribute__ ((sin usar)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project is about main, argc and argv.
