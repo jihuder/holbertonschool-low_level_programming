82bca58455663711f17c2fb8e0a424ed3f8730de
jihuder <julioarenas85@gmail.com>
2020-10-19 10:18:39 -0500

    added 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main Function
+ * @argc: Array of pointers
+ * @argv: Entered arguments
+ * Return: 0
+ */
+int  main(int argc, char *argv[])
+{
+if (argc == 3)
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
+else
+{
+printf("Error\n ");
+return (1);
+}
+return (0);
+}
