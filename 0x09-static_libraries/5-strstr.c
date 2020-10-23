6c8b090c6dde1c7469aa6b40680de77398667935
jihuder <julioarenas85@gmail.com>
2020-10-23 12:27:17 -0500

    added

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,23 +1,43 @@
 #include "holberton.h"
+
+
 /**
- * *_strpbrk - main function.
- * @s: the address of memory to print
- * @accept: the size of the memory to print
- * Return: s.
+ * _strstr - locates a substring
+ * @haystack: pointer - string to analize
+ * @needle: pointer - string to compare
+ *
+ * Return: pointer to the start of the coincidence
  */
-char *_strpbrk(char *s, char *accept)
+
+
+
+char *_strstr(char *haystack, char *needle)
 {
-int i;
-while (*s)
-{
-for (i = 0; accept[i]; i++)
-{
-if (accept[i] == *s)
-{
-return (s);
-}
-}
-s++;
-}
-return (0);
+  int x;
+  int y = 0;
+  int z = 0;
+
+  for (x = 0; haystack[x]; x++)
+    {
+      for (y = 0; needle[y]; y++)
+	{
+	  if (haystack[x] == needle[y])
+	    {
+	      break;
+	    }
+	}
+      for (; x < y; x++)
+	{
+	  if (haystack[x] == needle[y])
+	    {
+	      z++;
+	      y++;
+	    }
+	}
+      if (y == z)
+	{
+	  return (haystack + x);
+	}
+    }
+  return ('\0');
 }
