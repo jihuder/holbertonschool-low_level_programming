79e19d07965a014b6c0f8265e83453568df73dad
jihuder <julioarenas85@gmail.com>
2020-10-23 18:11:21 -0500

    added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,3 +1,11 @@
+#include "holberton.h"
+/**
+ * create_array  main funcion
+ * @size : size of the arrangement
+ * @c : pre-determined bytes
+ * Return: pointer
+ */
+
 char *create_array(unsigned int size, char c)
 {
 char *str;
@@ -9,7 +17,7 @@ return (NULL);
 else
 {
 unsigned int i = 0;
-for(i = 0; i < size; i ++)
+for (i = 0; i < size; i++)
 {
 str[i] = c;
 }
