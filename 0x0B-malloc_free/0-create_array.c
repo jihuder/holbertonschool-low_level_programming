29362d97492aa686b8da23019c6c68fec6890eff
jihuder <julioarenas85@gmail.com>
2020-10-23 16:56:38 -0500

    added 0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+char *create_array(unsigned int size, char c)
+{
+char *str;
+str = malloc(sizeof(str) * size);
+if (size == 0 || str == NULL)
+{
+return (NULL);
+}
+else
+{
+unsigned int i = 0;
+for(i = 0; i < size; i ++)
+{
+str[i] = c;
+}
+return (str);
+}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+this is procjest the malloc
