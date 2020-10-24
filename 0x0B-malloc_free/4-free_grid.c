9aef8eb78aae293a0aafd85b10c74802d19e9fdc
jihuder <julioarenas85@gmail.com>
2020-10-24 10:30:21 -0500

    added 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_grid - clean a two-dimensional matrix
+ * @grid: receive a pointer
+ * @height: get the height
+ * Return: void                                     --
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+int x;
+
+for (x = 0; x < height; x++)
+{
+free(grid[x]);
+}
+free(grid);
+}
