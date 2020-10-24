e423b7e9eec452745ab8cd23795920139ab25091
jihuder <julioarenas85@gmail.com>
2020-10-23 19:06:17 -0500

    actuality

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 
-	str = malloc(sizeof(str) * size);
+	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 	{
 		return (NULL);
