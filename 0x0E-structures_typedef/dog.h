42b4bea7c555f69c6e07b4f8cde82ce76f5b58a2
jihuder <julioarenas85@gmail.com>
2020-10-24 22:59:46 -0500

    added dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - date a dog
+ * @name: contains name
+ * @age: contains age
+ * @owner: contains owner
+ */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
-  
 #endif
