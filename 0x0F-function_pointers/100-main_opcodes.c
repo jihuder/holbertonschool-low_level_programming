f9293081b3a307b8c33dd79163b584b55aabe14e
jihuder <julioarenas85@gmail.com>
2020-10-26 15:10:13 -0500

    added 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print operation code
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 - success -- 1 - if the arguments are incorrect
+ * 2 - bytes are negative
+ **/
+
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+return (2);
+}
+return (0);
+}
