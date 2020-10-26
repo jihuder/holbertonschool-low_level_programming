bd76530c791bc50616048607df84f1096bb513f1
jihuder <julioarenas85@gmail.com>
2020-10-26 09:28:59 -0500

    added 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iteration to print the name
+ * @array:  array of character pointers
+ * @size: receives the number of times to be able to do the iteration
+ * @action: function to use
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x = 0;
+
+	if (array && size && action)
+	{
+		for (; x < size; x++)
+			action(array[x]);
+	}
+}
