1a85b7f0ce7dba4ab0ddbcb4cc67b717fd188819
jihuder <julioarenas85@gmail.com>
2020-10-26 14:43:46 -0500

    actuality

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+
+/**
+ * get_op_func - analize 2 place on argv to find operator
+ *
+ * @s: operator to analize
+ * Return: pointer to function corresponding to the rigth operator
+ */
+
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+
+int x = 0;
+
+while (ops[x].op)
+{
+if (*ops[x].op == *s)
+return (ops[x].f);
+x++;
+}
+return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: Count of arguments
+ * @argv: Array-vector of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+if (!get_op_func(argv[2]) || argv[2][1] != '\0')
+{
+printf("Error\n");
+return (99);
+}
+if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+{
+printf("Error\n");
+return (100);
+}
+printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+return (0);
+}
