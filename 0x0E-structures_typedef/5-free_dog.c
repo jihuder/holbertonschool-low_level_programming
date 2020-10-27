317b0f2c5460009a9a1e7efa05d6631e40042918
jihuder <julioarenas85@gmail.com>
2020-10-27 13:01:15 -0500

    added 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+
+#include "dog.h"
+#include <stdlib.h>
+
+
+/**
+ * free_dog - clean the memory of doggo
+ *
+ * @d: Aim at Doggo to be free
+ **/
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+
+free(d->name);
+free(d->owner);
+free(d);
+}
