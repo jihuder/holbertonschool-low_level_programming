d9de0553b10208baa8f84542b60b6f10679aa0f2
jihuder <julioarenas85@gmail.com>
2020-11-05 17:44:02 -0500

    added lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef LINKED_H_
+#ifndef LINKEND_H_
 #define LINKEND_H_
 
 #include <string.h>
@@ -15,8 +15,8 @@
  */
 typedef struct listint_s
 {
-  int n;
-  struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 
