3bf213cc617a331b50aafbddf588d191feea221d
jihuder <julioarenas85@gmail.com>
2020-11-05 17:36:11 -0500

    added

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - that prints all the elements of a listint_t list.
+ * @h: parameter analyzes header
+ *
+ * Return: nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+int i = 0;
+
+if (h == NULL)
+{
+return (0);
+}
+
+if (h)
+{
+while (h)
+{
+printf("%i\n", h->n);
+i++;
+h = h->next;
+}
+}
+return (i);
+}
