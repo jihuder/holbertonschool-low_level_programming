8ad5ebd8e5a11a36494f669dbdc752d1b4739d3c
jihuder <julioarenas85@gmail.com>
2020-11-24 14:07:35 -0500

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
+	while (text_content[count] != '\0')
+		count++;
 	stat = write(fd, text_content, count);
 	if (stat == -1)
 		return (-1);
