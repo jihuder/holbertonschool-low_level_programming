cae0dfe66eacb62b94f2a77d277e6b7889bf4e51
jihuder <julioarenas85@gmail.com>
2021-02-11 23:48:10 -0500

    added print_square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,27 @@
 #include "holberton.h"
+
 /**
  * print_square - prints the string "Holberton" from a character array.
  *@size: this is the variable
  * Return: 0 on success.
  */
+
 void print_square(int size)
 {
-int i;
-int j;
+	int i;
+	int j;
 
-if (size <= 0)
-{
-_putchar('\n');
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-_putchar(35);
-}
-_putchar('\n');
-}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
 }
