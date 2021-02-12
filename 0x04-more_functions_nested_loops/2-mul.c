e2351ae8df8b74c2b3c265848f3748d448721c9c
jihuder <julioarenas85@gmail.com>
2021-02-11 23:21:30 -0500

    added mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
  * mul - prints the string "Holberton" from a character array.
  *@a: this is the variable
  *@b: this is the variable
  * Return: c on success.
  */
+
 int mul(int a, int b)
 {
 
-int i = a * b;
+	int i = a * b;
 
-return (i);
+	return (i);
 }
