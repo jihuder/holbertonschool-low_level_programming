9454f700823ebcb436f36493be799554779581ce
jihuder <julioarenas85@gmail.com>
2021-02-11 23:39:36 -0500

    added print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,27 @@
 #include "holberton.h"
+
 /**
  * print_line - prints the string "Holberton" from a character array.
  *@n: this is the variable
  * Return: 0 on success.
  */
+
 void print_line(int n)
 {
 
-int j;
+	int j;
 
 
-if (n <= 0)
-{
-_putchar('\n');
-return;
-}
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
 
-for (j = 0; j < n; j++)
-{
-_putchar('_');
-}
-_putchar('\n');
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 }
