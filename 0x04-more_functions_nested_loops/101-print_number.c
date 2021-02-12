71c76257e3190fd2a1875e8c4ba8fc8b9c36b299
jihuder <julioarenas85@gmail.com>
2021-02-12 00:21:45 -0500

    added print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,30 +10,30 @@
 
 void print_number(int n)
 {
-unsigned int i, p, r;
+	unsigned int i, p, r;
 
-if (n < 0)
-{
-_putchar('-');
-i = n * -1;
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		i = n * -1;
+	}
 
-else
-{
-i = n;
-}
+	else
+	{
+		i = n;
+	}
 
-p = 1;
-r = i;
+	p = 1;
+	r = i;
 
-while (r > 9)
-{
-p *= 10;
-r /= 10;
-}
+	while (r > 9)
+	{
+		p *= 10;
+		r /= 10;
+	}
 
-for (; p >= 1; p /= 10)
-{
-_putchar(((i / p) % 10) + '0');
-}
+	for (; p >= 1; p /= 10)
+	{
+		_putchar(((i / p) % 10) + '0');
+	}
 }
