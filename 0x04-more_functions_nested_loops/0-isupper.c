007af0565c6eab290642cbec82fa9e63df9fcd48
jihuder <julioarenas85@gmail.com>
2021-02-11 23:02:31 -0500

    added isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,23 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
  * _isupper - prints the string "Holberton" from a character array.
- *@c: this is the variable
+ * @c: this is the variable
  * Return: 0 on success.
  */
+
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
 
-return (0);
+	if(c >= 65)
+	{
+		if (c <= 97)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
 }
