51d0aab2f3926b27893ba547126a0da6743c4436
jihuder <julioarenas85@gmail.com>
2021-02-16 21:16:36 -0500

    added 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
+
 /**
  * print_rev - print backwards
  * @s: variable printing variable
  * Return: void
  */
+
 void print_rev(char *s)
 {
-int i;
-while (*s != '\0')
-{
-i++;
-s++;
-}
-i--;
-while (i >= 0)
-{
-s--;
-i--;
-_putchar(*s);
-}
-_putchar('\n');
+	int i;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		s--;
+		i--;
+		_putchar(*s);
+	}
+	_putchar('\n');
 }
