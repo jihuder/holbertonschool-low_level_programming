3356079caaf360efd44b1c175e7b1140c58c8eeb
jihuder <julioarenas85@gmail.com>
2021-02-16 21:33:47 -0500

    added 7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
+
 /**
  * puts_half - print Print every 2 odd characters
  * @str: variables - char pointer to string
  * Return: void.
  */
+
 void puts_half(char *str)
 {
-int i = 0;
-int div;
-for (i = 0; str[i] != '\0';  i++)
-{
-i++;
-}
-div = i / 2;
-div = i - div;
-while (str[div] != '\0')
-{
-_putchar(str[div]);
-div++;
-}
-_putchar('\n');
+	int i = 0;
+	int div;
+
+	for (i = 0; str[i] != '\0';  i++)
+	{
+		i++;
+	}
+	div = i / 2;
+	div = i - div;
+
+	while (str[div] != '\0')
+	{
+		_putchar(str[div]);
+		div++;
+	}
+	_putchar('\n');
 }
