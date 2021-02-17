1ec36c52707d2db76013d8e6c0a636bfe684551a
jihuder <julioarenas85@gmail.com>
2021-02-16 20:50:24 -0500

    added 1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
+
 /**
  * swap_int - change values
  * @a: variable
  * @b: variable
  */
+
 void swap_int(int *a, int *b)
 {
-int aux;
-aux = *a;
-*a = *b;
-*b = aux;
+	int aux;
+	aux = *a;
+	*a = *b;
+	*b = aux;
 }
