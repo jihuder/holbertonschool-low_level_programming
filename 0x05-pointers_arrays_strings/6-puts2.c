d8d06bd2f29d1b65ce990fa0716ae9c453625575
jihuder <julioarenas85@gmail.com>
2021-02-16 21:25:32 -0500

    added 6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
+
 /**
  * puts2- print Print every 2 characters
  * @str: variables - char pointer to string
  * Return: void.
  */
+
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-if (i % 2 == 0)
-{
-_putchar(str[i]);
-}
-i++;
-}
-_putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
