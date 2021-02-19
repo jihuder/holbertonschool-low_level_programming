7a8e34c02297e4ec2632d40a7cebba0b220deaa1
jihuder <julioarenas85@gmail.com>
2021-02-18 23:08:42 -0500

    added rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
+
 /**
  * reverse_array - check the code of holberton school students
  * @a: this is the first accountant
  * @n: this is the second accountant
  * Return: Always 0.
  */
+
 void reverse_array(int *a, int n)
 {
-int i = 0;
-int b = 0;
-for (i = 0; i < n; i++)
-{
-b = a[i];
-a[i] = a[n - 1];
-a[--n] = b;
-}
+	int i = 0;
+	int b = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		b = a[i];
+		a[i] = a[n - 1];
+		a[--n] = b;
+	}
 }
