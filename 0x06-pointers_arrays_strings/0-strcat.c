3c55390eef1650df53e64bf30a72aaccafd51cdc
jihuder <julioarenas85@gmail.com>
2021-02-18 22:51:37 -0500

    added strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,12 @@ char *_strcat(char *dest, char *src)
 {
 	int count1;
 	int count2;
+
 	for (count1 = 0; dest[count1] != '\0'; count1++)
 		;
 	for (count2 = 0; src[count2] != '\0'; count2++)
 	{
 		dest[count1 + count2] = src[count2];
 	}
-return (dest);
+	return (dest);
 }
