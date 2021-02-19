515ba5525f9376e7e3f443f90a87c9ef5fbb25d6
jihuder <julioarenas85@gmail.com>
2021-02-18 22:58:54 -0500

    added strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * *_strncpy - check the code of holberton school students
  * @dest: this is the first accountant.
@@ -6,16 +7,19 @@
  * @n: this is the third accountant.
  * Return: Always 0.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-for (; i < n; i++)
-{
-dest[i] = '\0';
-}
-return (dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
 }
