070062813cbb5239669896e70477aa166e5df3c3
jihuder <julioarenas85@gmail.com>
2021-02-23 00:11:20 -0500

    added print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,23 @@
 #include "holberton.h"
+
 /**
  * print_chessboard - print a chessboard.
  * @a: matriz
  * Return: Nothing
  */
+
 void print_chessboard(char (*a)[8])
 {
-int i, f, g;
-g = sizeof(a);
-for (i = 0; i < g ; i++)
-{
-for (f = 0; f < g; f++)
-{
-_putchar(a[i][f]);
-}
-_putchar('\n');
-}
+	int i, f, g;
+
+	g = sizeof(a);
+
+	for (i = 0; i < g ; i++)
+	{
+		for (f = 0; f < g; f++)
+		{
+			_putchar(a[i][f]);
+		}
+		_putchar('\n');
+	}
 }
