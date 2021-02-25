60f691228743b00d54a90c24b71a221d51136230
jihuder <julioarenas85@gmail.com>
2021-02-24 23:45:37 -0500

    added is_palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
+
 /**
  * compare_palindrome - prints buffer in hexa
  * @p: the address of memory to print
  * Return: s.
  */
+
 int compare_palindrome(char *p)
 {
-return (*p ? 1 + compare_palindrome(p + 1) : 0);
+	return (*p ? 1 + compare_palindrome(p + 1) : 0);
 }
 
 /**
@@ -16,35 +18,38 @@ return (*p ? 1 + compare_palindrome(p + 1) : 0);
  * @p: the address of memory to print
  * Return: s.
  */
+
 int solve_palindrome(char *p, int cont, int len)
 {
-if (p[cont] == p[len] && p[cont] != '\0')
+	if (p[cont] == p[len] && p[cont] != '\0')
 {
-return (1 * solve_palindrome(p, cont + 1, len - 1));
+	return (1 * solve_palindrome(p, cont + 1, len - 1));
 }
-else if (p[cont] == '\0' && p[len] == '\0')
-{
-return (1);
+	else if (p[cont] == '\0' && p[len] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-else
-{
-return (0);
-}
-}
+
 /**
  * is_palindrome - prints buffer in hexa
  * @s: the address of memory to print
  * Return: s.
  */
+
 int is_palindrome(char *s)
 {
-int len;
+	int len;
 
-len = compare_palindrome(s);
-len--;
-if (len == 0)
-{
-return (1);
-}
-return (solve_palindrome(s, 0, len));
+	len = compare_palindrome(s);
+	len--;
+	if (len == 0)
+	{
+		return (1);
+	}
+	return (solve_palindrome(s, 0, len));
 }
