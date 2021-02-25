5049712725c6233a71cb5d159cea516b07886e11
jihuder <julioarenas85@gmail.com>
2021-02-24 23:27:03 -0500

    added strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
  * _strlen_recursion - prints buffer in hexa
  * @s: the address of memory to print
  * Return: s.
  */
+
 int _strlen_recursion(char *s)
 {
-if (*s != '\0')
-{
-return (1 + _strlen_recursion(s + 1));
-}
-return (0);
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
