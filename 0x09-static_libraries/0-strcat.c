0f80ffeac0fab2b2e63249500a8e6ec35b4def07
jihuder <julioarenas85@gmail.com>
2021-02-28 22:35:11 -0500

    added 0-strcat

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
+
 /**
  * *_strcat - check the code of holberton school students
  * @dest: thi is variable 1
  * @src: this is variable 2
  * Return: always 0.
  */
+
 char *_strcat(char *dest, char *src)
 {
-int count1;
-int count2;
-for (count1 = 0; dest[count1] != '\0'; count1++)
-;
-for (count2 = 0; src[count2] != '\0'; count2++)
-{
-dest[count1 + count2] = src[count2];
-}
-return (dest);
+	int count1;
+	int count2;
+	for (count1 = 0; dest[count1] != '\0'; count1++)
+		;
+	for (count2 = 0; src[count2] != '\0'; count2++)
+	{
+		dest[count1 + count2] = src[count2];
+	}
+	return (dest);
 }
