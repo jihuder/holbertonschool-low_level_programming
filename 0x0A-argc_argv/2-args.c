e079888bd71a4cf4cd6e718a897d9fc96ed1cf72
jihuder <julioarenas85@gmail.com>
2021-02-28 20:31:42 -0500

    added 2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - Main Function
  * @argc: Array of pointers
  * @argv: Entered arguments
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-int i;
+	int i;
 
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
