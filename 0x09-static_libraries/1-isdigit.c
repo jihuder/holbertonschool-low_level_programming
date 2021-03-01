a4533bca8dd37dbf3ea6b790915deb213248d3c1
jihuder <julioarenas85@gmail.com>
2021-02-28 22:38:21 -0500

    added 1-isdigit

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
  * _isdigit - prints the string "Holberton" from a character array.
  *@c: this is the variable
  * Return: 0 on success.
  */
+
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-return (0);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
 }
