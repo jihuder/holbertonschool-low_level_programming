cfb40a303a7ac73dc017bf0c81e50ca671903e6e
jihuder <julioarenas85@gmail.com>
2021-02-28 22:44:59 -0500

    added 1-strncat

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * *_strncat - check the code of holberton school students
  * @dest: this is accountant
@@ -6,15 +7,17 @@
  * @n: this is the third accountant
  * Return: always 0
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int b;
-for (i = 0; *(dest + i) != '\0'; i++)
-;
-for (b = 0; b < n && src[b] != '\0'; b++)
-{
-dest[i + b] = src[b];
+	int i;
+	int b;
+	for (i = 0; *(dest + i) != '\0'; i++)
+		;
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[i + b] = src[b];
+	}
+	return (dest);
 }
-return (dest);
-}
+
