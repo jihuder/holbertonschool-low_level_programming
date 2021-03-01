edd0124a66a8e116e581d1f983d7d6100563109a
jihuder <julioarenas85@gmail.com>
2021-02-28 23:25:58 -0500

    added 9-strcpy

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,17 +1,19 @@
 #include "holberton.h"
+
 /**
  * _strcpy - check the code of holberton school students.
  * @src: the variable
  * @dest: another variable
  * Return: the ariable finished
  */
+
 char *_strcpy(char *dest, char *src)
 {
-int i;
-for (i = 0; src[i]; i++)
-{
-dest[i] = src[i];
-}
-dest[i] = src[i];
-return (dest);
+	int i;
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = src[i];
+	return (dest);
 }
