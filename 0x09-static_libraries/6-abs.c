d2c183c20b705841dce1581b7d42ef6d4f1e6c66
jihuder <julioarenas85@gmail.com>
2021-02-28 23:23:36 -0500

    added 6-abs

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
  *_abs - unction that computes the absolute value of an integer.
  *@n: this variable
  *Return:  (n)  Always
  */
+
 int _abs(int n)
 {
-if (n < 0)
-{
-n = n * -1;
-}
-return (n);
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
 }
