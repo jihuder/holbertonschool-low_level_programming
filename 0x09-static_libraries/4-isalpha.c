c429dc26f56c0fb18ca2858c36cb987acd752e67
jihuder <julioarenas85@gmail.com>
2021-02-28 23:10:58 -0500

    added 4-isalpha

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,22 +1,24 @@
 #include "holberton.h"
+
 /**
  * _isalpha - print from the a to the z
  * @c: Give me the number
  * Return: Always 0
  */
+
 int _isalpha(int c)
 {
-int n;
-int m;
-for (m = 'A'; m <= 'Z'; m++)
-{
-for (n = 'a'; n <= 'z'; n++)
-{
-if (c >= m && c <= n)
-{
-return (1);
-}
-}
-}
-return (0);
+	int n;
+	int m;
+	for (m = 'A'; m <= 'Z'; m++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			if (c >= m && c <= n)
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
