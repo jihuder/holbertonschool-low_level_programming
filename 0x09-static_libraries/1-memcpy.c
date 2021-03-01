18da13711654a043bfbd2909fa72f7cf79551ea8
jihuder <julioarenas85@gmail.com>
2021-02-28 22:40:48 -0500

    added 1-memcpy

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * *_memcpy - prints buffer in hexa
  * @dest: the address of memory to print
@@ -6,12 +7,13 @@
  * @n: the size of the memory to print
  * Return: dest.
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int c;
-for (c = 0; c < n; c++)
-{
-dest[c] = src[c];
-}
-return (dest);
+	unsigned int c;
+	for (c = 0; c < n; c++)
+	{
+		dest[c] = src[c];
+	}
+	return (dest);
 }
