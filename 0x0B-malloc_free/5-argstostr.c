c7d70c8b150a9aa40a31cd8075ec22e0fa37814e
jihuder <julioarenas85@gmail.com>
2021-03-02 22:58:52 -0500

    added 5-argstostr

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,43 +10,44 @@
  * to new array
  *
  */
+
 char *argstostr(int ac, char **av)
 {
-char *arr;
-int x = 0, y, len = 0, p = 0;
+	char *arr;
+	int x = 0, y, len = 0, p = 0;
 
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
-while (av[x])
-{
-y = 0;
-while (av[x][y])
-{
-len++;
-y++;
-}
-x++;
-}
+	while (av[x])
+	{
+		y = 0;
+		while (av[x][y])
+		{
+			len++;
+			y++;
+		}
+		x++;
+	}
 
-arr = malloc(len + ac + 1);
+	arr = malloc(len + ac + 1);
 
-if (arr == NULL)
-{
-return (NULL);
-}
-for (x = 0; x < ac; x++)
-{
-for (y = 0; av[x][y]; y++)
-{
-arr[p] = av[x][y];
-p++;
-}
-arr[p] = '\n';
-p++;
-}
-arr[p] = '\0';
-return (arr);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y]; y++)
+		{
+			arr[p] = av[x][y];
+			p++;
+		}
+		arr[p] = '\n';
+		p++;
+	}
+	arr[p] = '\0';
+	return (arr);
 }
