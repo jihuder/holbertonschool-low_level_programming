f4cc7d7fadeae34110e9b2ee318b70695340a6e1
jihuder <julioarenas85@gmail.com>
2021-03-02 22:36:35 -0500

    added str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,37 +12,39 @@
 
 char *str_concat(char *s1, char *s2)
 {
-char *str;
-unsigned int x, y;
-unsigned int len1 = 0, len2 = 0;
+	char *str;
+	unsigned int x, y;
+	unsigned int len1 = 0, len2 = 0;
 
-if (s1 != NULL)
-{
-while (s1[len1])
-len1++;
-}
+	if (s1 != NULL)
+	{
+		while (s1[len1])
+			len1++;
+	}
 
-if (s2 != NULL)
-{
-while (s2[len2])
-len2++;
-}
+	if (s2 != NULL)
+	{
+		while (s2[len2])
+			len2++;
+	}
 
-str = malloc((sizeof(char) * (len1 + len2))+1);
+	str = malloc((sizeof(char) * (len1 + len2))+1);
 
-if (str == NULL)
-{
-return (NULL);
-}
-for (x = 0; x < len1; x++)
-{
-str[x] = s1[x];
-}
-for (y = 0; y < len2; y++)
-{
-str[x] = s2[y];
-x++;
-}
-str[x] = '\0';
-return (str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < len1; x++)
+	{
+		str[x] = s1[x];
+	}
+	for (y = 0; y < len2; y++)
+	{
+		str[x] = s2[y];
+		x++;
+	}
+	str[x] = '\0';
+
+	return (str);
+
 }
