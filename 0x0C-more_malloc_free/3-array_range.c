0d4345e85ff7912edc485c86972e7cb286e6fc5d
jihuder <julioarenas85@gmail.com>
2021-03-04 23:28:51 -0500

    added array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,22 +13,22 @@
 
 int *array_range(int min, int max)
 {
-int *arr;
-int x = 0;
+	int *arr;
+	int x = 0;
 
-if (min > max)
-{
-return (NULL);
-}
-arr = malloc(sizeof(int) * (max - min + 1));
-if (arr == NULL)
-{
-return (NULL);
-}
-for (; min <= max; x++)
-{
-arr[x] = min;
-min++;
-}
-return (arr);
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(int) * (max - min + 1));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (; min <= max; x++)
+	{
+		arr[x] = min;
+		min++;
+	}
+	return (arr);
 }
