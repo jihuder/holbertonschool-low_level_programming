0eb1f042be67c4f725c57d60d05cb10f611c8f93
jihuder <julioarenas85@gmail.com>
2021-03-08 23:46:25 -0500

    added 4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,37 +13,40 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *doggo;
-unsigned int len1 = 0, len2 = 0, x, y;
+	dog_t *doggo;
+	unsigned int len1 = 0, len2 = 0, x, y;
 
-while (name[len1])
-len1++;
-while (owner[len2])
-len2++;
-doggo = malloc(sizeof(dog_t));
+	while (name[len1])
+		len1++;
+	while (owner[len2])
+		len2++;
+	doggo = malloc(sizeof(dog_t));
 
-if (doggo == NULL)
-return (NULL);
+	if (doggo == NULL)
+		return (NULL);
 
-doggo->name = malloc(sizeof(char) * (len1 + 1));
-if (doggo->name == NULL)
-{
-free(doggo);
-return (NULL);
-}
-for (x = 0; x <= len1; x++)
-doggo->name[x] = name[x];
-doggo->age = age;
+	doggo->name = malloc(sizeof(char) * (len1 + 1));
+	if (doggo->name == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
 
-doggo->owner = malloc(sizeof(char) * (len2 + 1));
-if (doggo->owner == NULL)
-{
-free(doggo->name);
-free(doggo);
-return (NULL);
-}
-for (y = 0; y <= len2; y++)
-doggo->owner[y] = owner[y];
+	for (x = 0; x <= len1; x++)
+		doggo->name[x] = name[x];
+	doggo->age = age;
 
-return (doggo);
+	doggo->owner = malloc(sizeof(char) * (len2 + 1));
+
+	if (doggo->owner == NULL)
+	{
+		free(doggo->name);
+		free(doggo);
+		return (NULL);
+	}
+	for (y = 0; y <= len2; y++)
+		doggo->owner[y] = owner[y];
+
+
+	return (doggo);
 }
