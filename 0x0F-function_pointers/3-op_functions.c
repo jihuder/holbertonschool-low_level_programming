709fcf4a692fbc7114730bdef805f92a5bd84f47
jihuder <julioarenas85@gmail.com>
2021-03-10 23:06:39 -0500

    added op_functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,7 +9,7 @@
 
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
@@ -22,7 +22,7 @@ return (a + b);
 
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
@@ -35,7 +35,7 @@ return (a - b);
 
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
@@ -48,7 +48,7 @@ return (a * b);
 
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
 
 /**
@@ -61,5 +61,5 @@ return (a / b);
 
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
