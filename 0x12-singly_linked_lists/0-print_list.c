5c63778093bf11f9ffcaaf9a8a02c5f8bf1f3de9
jihuder <julioarenas85@gmail.com>
2021-03-18 22:47:26 -0500

    [update ~ 0-print_list.c](update file)

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,20 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int n = 0;
+
 	if (h != NULL)
 	{
-		while(h != NULL)
+		while (h != NULL)
 		{
-			if(h->str == NULL)
+			if (h->str == NULL)
+			{
 				printf("[0] (nill)\n");
-			else
-				printf("[%d] %s\n" h->len, h-str);
 
+			}
+			else
+			{
+				printf("[%d] %s\n", h->len, h->str);
+			}
 			h = h->next;
 			n++;
 		}
