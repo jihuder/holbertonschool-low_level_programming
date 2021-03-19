5d16998c8ce2aea669eef8df4baa65783d5ea672
jihuder <julioarenas85@gmail.com>
2021-03-18 23:52:05 -0500

    [update ~ lists.h](update file)

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H
+#define _LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,6 +20,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+void before_main(void) __attribute__ ((constructor));
+int _putchar(char c);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
