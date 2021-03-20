d94e7c8bff222a7d7286c0144750db2c0de277ba
jihuder <julioarenas85@gmail.com>
2021-03-19 19:56:08 -0500

    [update ~ 0-print_list.c](Update file)

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,30 +4,30 @@
  * print_list -imprint list.
  * @h: It enters as a parameter a list.
  *
- * Return : The number of nodes.
+ * Return: The number of nodes.
  */
 
 size_t print_list(const list_t *h)
 {
 	unsigned int n = 0;
 
-	if (h != NULL)
+	if (h == NULL)
 	{
-		while (h != NULL)
-		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nill)\n");
-
-			}
-			else
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
-			h = h->next;
-			n++;
-		}
-		return (n);
+		return (0);
 	}
-	return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
