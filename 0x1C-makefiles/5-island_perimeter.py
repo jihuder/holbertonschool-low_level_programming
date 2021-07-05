1452466f19613eef7554c31e5425d4f69f6ec454
jiuder <julioarenas85@gmail.com>
2021-07-04 21:19:57 -0500

    [Feat ~island_perimeter](Return perimeter)

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Module _ returns the perimeter of the island
+"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of grid"""
+    tol = 0
+
+    for i in range(leng(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] is 1:
+                tol +=4
+                if j > 0 and grid[i][j -1]:
+                    tol -= 2
+                if i > 0 and grid [i - 1]:
+                    total -= 2
